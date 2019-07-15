f9e5657275ba19b1b372a557bb1c80623472913e
EnriqueInteli-k <enrique.madrigal@inteli-k.com>
2019-07-15 08:59:08 -0500

    Nuevos hoy..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -260,7 +260,30 @@
                     </div>
 
 <div class="w-100 clearfix" style="background: #4cbd94; height: 480px;">
+<div class="row">
+<div class="col-lg-1"></div> 
+<div class="col-lg-3">
+<p class="lomelin-text10">Lomelín Bienes Raíces</p>
+<p class="lomelin-text9">Asesoramos a nuestros clientes con honradez y responsabilidad para brindarles las mejores soluciones y servicios inmobiliarios, en la promoción de inmuebles a la venta o renta, administración, consultoría y comercialización de desarrollos.</p>
+</div> 
 
+<div class="col-lg-1"></div> 
+<div class="col-lg-3">
+<p class="lomelin-text10">Contácto</p>
+<p class="lomelin-text9">Central Park torre Corporativo 2 5o Piso 5C-1 Av. Armando Birlain Schafler Centro Sur Querétaro, México</p>
+<p class="lomelin-text9">Horarios Lunes - Viernes: 9 am - 5 pm Sábado: 9 am - 1pm Domingo: Cerrado +52 (442) 216-33-94</p>
+</div> 
+
+<div class="col-lg-1"></div> 
+<div class="col-lg-3">
+<p class="lomelin-text10">Boletín</p>
+<p class="lomelin-text9">Suscríbete a nuestro boletín! no te pierdas las últimas ofertas inmobiliarias y noticias de nuevos desarrollos.</p>
+<p class="lomelin-text9"><input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username"></p>
+<button type="button" class="btn btn-dark btn-fw">Enviar</button>
+</div> 
+
+
+</div>
 
 </div>
 
